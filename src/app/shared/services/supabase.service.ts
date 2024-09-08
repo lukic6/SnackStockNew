d68e321c4aee62f16fe7287cbf4284bb9a5aa91c
Luka Korenčan <lkorencan@gmail.com>
2024-09-08 11:10:47 +0200

    dodan supabase modul, generiran supabase service, ki bo injectan v komponente in pripravljen file s creds-i - dodan v .gitignore

---
+++ src/app/shared/services/supabase.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { supabaseKey, supabaseUrl } from '../../../supabase-creds';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupabaseService {
+  private supabase: SupabaseClient;
+  constructor() { 
+    this.supabase = createClient(supabaseUrl, supabaseKey);
+  }
+}
